39ee492cbc1a4d909f2bdd43bedf11d6d214c191
akshat-61 <akshat74269@gmail.com>
2024-10-25 00:16:33 +0530

    code.commit

--- code.cpp
+++ code.cpp
@@ -50,6 +50,49 @@ int main()
     }
     return 0;
 
+    void printboard()
+    {
+        cout << "\n";
+        for (int i = 0; i < 9; i++)
+        {
+            cout << " " << board[i];
+
+            if ((i + 1) % 3 == 0)
+            {
+                cout << "\n";
+                if (i != 8)
+                    cout << "---|---|---\n";
+            }
+            else
+            {
+                cout << " |";
+            }
+        }
+        cout << "\n";
+    }
+
+    bool checkwinner(const vector<char> &b, char player)
+    {
+
+        const vector<vector<int>> winningcombos = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}};
+        for (auto combo : winningcombos)
+        {
+            if (b[combos[0]] == player && b[combo[1]] == player && b[combo[2]] == player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     void resetgame()
     {
         board = vector<char>(9, empty);
