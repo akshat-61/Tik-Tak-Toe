b5c3522cf7b4581bcbe4593df6e813a0ec9f6f5e
akshat-61 <akshat74269@gmail.com>
2024-10-24 21:57:14 +0530

    code1.commit

--- code.cpp
+++ code.cpp
@@ -15,6 +15,41 @@ int main()
     cout << "Tic Tac Toe game with AI";
     resetgame();
 
+    while (true)
+    {
+        printboard();
+        if (!checkwinner(board, Player) && !checkwinner(board, Ai) && !checktie(board))
+        {
+            playermove();
+
+            if (!checkwinner(board, Player) && !checktie(board))
+            {
+                int move = bestmove();
+                board[move] = AI;
+                cout << "AI played at position " << move + 1 << endl;
+            }
+        }
+        if (checkwinner(board, Player))
+        {
+            printboard();
+            cout << "Player Wins! ";
+            break;
+        }
+        else if (checkwinner(board, Ai))
+        {
+            printboard();
+            cout << "AI Wins! ";
+            break;
+        }
+        else if (checktie(board))
+        {
+            printboard();
+            cout << "The match is Tie! ";
+            break;
+        }
+    }
+    return 0;
+
     void resetgame()
     {
         board = vector<char>(9, empty);
